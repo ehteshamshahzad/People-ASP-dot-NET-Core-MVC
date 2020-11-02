b6452c57dad5e926311af93731540819929400b3
Ehtesham Shahzad <ehteshamshahzad@hotmail.com>
2020-11-02 19:24:12 +0500

    Database migration

---
+++ Data/PeopleContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using People.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace People.Data
+{
+    public class PeopleContext : DbContext
+    {
+        public PeopleContext(DbContextOptions<PeopleContext> options)
+        : base(options) { }
+        
+        public DbSet<Person> People { get; set; }
+    }
+}
