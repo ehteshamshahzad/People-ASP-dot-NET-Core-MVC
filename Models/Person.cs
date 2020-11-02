c038aac183ca771f586681ccc101e57f934412db
Ehtesham Shahzad <ehteshamshahzad@hotmail.com>
2020-11-02 17:23:25 +0500

    Person model class added

---
+++ Models/Person.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace People.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [StringLength(100)]
+        public string Address { get; set; }
+    }
+}
