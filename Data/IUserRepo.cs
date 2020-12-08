afd406d60d0e8cdb8ecf2597d7516868c36e5fe4
Ehtesham Shahzad <ehteshamshahzad@hotmail.com>
2020-12-08 15:05:30 +0500

    Repository added

---
+++ Data/IUserRepo.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using People.Models;
+
+namespace People.Data
+{
+    public interface IUserRepo
+    {
+        User CreateUser(User user);
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(int id);
+        User UpdateUser(User user);
+        User DeleteUser(int id);
+    }
+}
