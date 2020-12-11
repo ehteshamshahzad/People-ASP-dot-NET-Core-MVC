8c6f86db11ef95c21de6714155fb4f0d8b91debd
Ehtesham Shahzad <ehteshamshahzad@hotmail.com>
2020-12-11 15:24:52 +0500

    Fixing security bug

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using People.Models;
 
 namespace People.Controllers
 {
+    [Authorize]
     public class UsersController : Controller
     {
         private readonly UsersContext _context;
